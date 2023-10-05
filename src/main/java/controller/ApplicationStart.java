82b0492a8cdc1168fb03b8d1e1b8a1b44aa7e033
Anizme <hirengtlesone@gmail.com>
2023-10-06 00:05:52 +0700

    Them logo sieu xin xo

--- src/main/java/controller/ApplicationStart.java
+++ src/main/java/controller/ApplicationStart.java
@@ -4,6 +4,7 @@ import dictionary.DictionaryManagement;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -21,8 +22,8 @@ public class ApplicationStart extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ApplicationStart.class.getResource("container.fxml"));
+        stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/images/RGB_LOGO.png")));
         Scene scene = new Scene(fxmlLoader.load(), 930, 600);
-        //stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
