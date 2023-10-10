5a4bd78ddd1e1e928464be7316ce47fe43562fee
Anizme <hirengtlesone@gmail.com>
2023-10-10 12:07:48 +0700

    Chinh code cho sort (class Word implements Comparable de su dung Collections)

--- src/main/java/algorithms/Sort.java
+++ src/main/java/algorithms/Sort.java
@@ -2,15 +2,11 @@ package algorithms;
 
 import dictionary.Word;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 public class Sort {
     public static void sortDictionaryInAlphabeticalOrder(List<Word> listOfWords) {
-        listOfWords.sort(new Comparator<Word>() {
-            @Override
-            public int compare(Word w1, Word w2) {
-                return w1.getWord_target().toLowerCase().compareTo(w2.getWord_target().toLowerCase());
-            }
-        });
+        Collections.sort(listOfWords);
     }
 }
