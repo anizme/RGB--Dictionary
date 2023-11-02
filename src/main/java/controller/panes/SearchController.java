0556a28b0f689052133b210b55b109b37c19f38a
chuquangcan <chuquangcan231@gmail.com>
2023-11-03 01:00:06 +0700

    fix enter to search; add history search;

--- src/main/java/controller/panes/SearchController.java
+++ src/main/java/controller/panes/SearchController.java
@@ -12,6 +12,8 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.HTMLEditor;
@@ -20,7 +22,10 @@ import services.DatabaseConnect;
 import services.VoiceRSS;
 
 import java.net.URL;
+import java.util.Collections;
 import java.util.ResourceBundle;
+
+import java.util.Stack;
 
 import static controller.ApplicationStart.dictionaryManagement;
 
@@ -44,9 +49,12 @@ public class SearchController extends ActionController implements Initializable 
     @FXML
     private Button btSave;
 
+    private Stack<String> history = new Stack<>();
+
+    private boolean check = true;
+
     @FXML
     void usSpeak(ActionEvent event) throws Exception {
-
         VoiceRSS.Name = VoiceRSS.voiceNameUS;
         speak("en-gb");
     }
@@ -66,6 +74,7 @@ public class SearchController extends ActionController implements Initializable 
     void lookup(ActionEvent event) {
         wvMeaning.getEngine().loadContent(DatabaseConnect.getMeaning(tfSearchWord.getText()));
         //wvMeaning.setText(dictionaryManagement.dictionaryLookup(tfSearchWord.getText()));
+        history.push(tfSearchWord.getText() + "  ⓡ");
     }
 
     @FXML
@@ -129,9 +138,16 @@ public class SearchController extends ActionController implements Initializable 
             lvSearchWordsList.getItems().clear();
             if (tfSearchWord.getText() != null) {
                 String tmp = tfSearchWord.getText();
-                String querry = String.format("SELECT word FROM av WHERE word LIKE '%s%%' ORDER BY word", tmp);
-                lvSearchWordsList.getItems()
-                        .addAll(DatabaseConnect.GetWord(querry));
+                if (!tmp.equals("")) {
+                    String querry = String.format("SELECT word FROM av WHERE word LIKE '%s%%' ORDER BY word", tmp);
+                    lvSearchWordsList.getItems()
+                            .addAll(DatabaseConnect.GetWord(querry));
+                } else {
+                    Collections.reverse(history);
+                    lvSearchWordsList.getItems().addAll(history);
+                    Collections.reverse(history);
+                    check = false;
+                }
             }
         });
         lvSearchWordsList.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -141,9 +157,33 @@ public class SearchController extends ActionController implements Initializable 
                     tfSearchWord.setText(lvSearchWordsList.getSelectionModel().getSelectedItem());
                     //wvMeaning.setText(dictionaryManagement.dictionaryLookup(tfSearchWord.getText()));
                     wvMeaning.getEngine().loadContent(DatabaseConnect.getMeaning(tfSearchWord.getText()));
+                    history.push(tfSearchWord.getText() + "  ⓡ");
                 }
             }
         });
+
+        tfSearchWord.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (tfSearchWord.getText().isEmpty() && check) {
+                    Collections.reverse(history);
+                    lvSearchWordsList.getItems().addAll(history);
+                    Collections.reverse(history);
+                    check = false;
+                } else if (!tfSearchWord.getText().isEmpty()) {
+                    check = true;
+                }
+            }
+        });
+
+        tfSearchWord.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.ENTER) {
+                    lookup(null);
+                }
+            }
+        });
+
         htmlUpdateMeaning.setVisible(false);
         btSave.setVisible(false);
     }
