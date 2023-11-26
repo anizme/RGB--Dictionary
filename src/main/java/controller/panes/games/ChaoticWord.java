f2dc2d85eb3f6dc0b12e67c9273a3f6bebb5463e
chuquangcan <chuquangcan231@gmail.com>
2023-11-26 23:43:15 +0700

    chaoticWord game final first demo

--- src/main/java/controller/panes/games/ChaoticWord.java
+++ src/main/java/controller/panes/games/ChaoticWord.java
@@ -9,6 +9,7 @@ import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 
@@ -17,6 +18,7 @@ import javafx.util.Duration;
 
 import javafx.scene.shape.Rectangle;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
@@ -41,12 +43,33 @@ public class ChaoticWord extends GameController implements Initializable {
     private AnchorPane upAnchorPane;
 
     @FXML
+    private AnchorPane defaultAnchorpane;
+
+
+    @FXML
     private Rectangle downRectangle;
 
     @FXML
     private Rectangle upRectangle;
 
+    @FXML
+    private JFXButton playButton;
+
+    @FXML
+    private JFXButton replayButton;
+
+    @FXML
+    private JFXButton submitButton;
+
+    @FXML
+    private TextField meaningLabel;
+
+    @FXML
+    private TextField meaningTextField;
+
     private int numsOfLetter = 0;
+    private final double weighD = 350;
+    private final double heightD = 60;
     private final int sizeCardX = 50;
     private final int sizeCardY = 50;
     private final int padding = 5;
@@ -54,11 +77,16 @@ public class ChaoticWord extends GameController implements Initializable {
     private List<Word> listWord;
     private List<Integer> upCheckIndex;
     private List<Integer> downCheckIndex;
+    private List<String> rawAns;
+    private List<JFXButton> allButton;
     private Set<Integer> checkExists;
+    private Map<JFXButton, Integer> memoryPosition;
     private String wordTarget;
     private String wordMeaning;
     private String wordPlay;
+    private String playerAns = "";
     private int sizeOfHBox;
+    private boolean isTimelineRunning = false;
 
     public ChaoticWord() throws FileNotFoundException {
         listWord = new ArrayList<>();
@@ -66,6 +94,9 @@ public class ChaoticWord extends GameController implements Initializable {
         position = new ArrayList<>();
         upCheckIndex = new ArrayList<>();
         downCheckIndex = new ArrayList<>();
+        memoryPosition = new HashMap<>();
+        rawAns = new ArrayList<>();
+        allButton = new ArrayList<>();
         File dictionaryFile = new File("src/main/resources/data/dictionaries.txt");
         Scanner sc = new Scanner(dictionaryFile);
         while (sc.hasNextLine()) {
@@ -80,24 +111,32 @@ public class ChaoticWord extends GameController implements Initializable {
     }
 
     public void initGame() {
+        playButton.setVisible(false);
+        replayButton.setVisible(true);
+        submitButton.setVisible(true);
+        meaningLabel.setVisible(true);
+        meaningTextField.setVisible(true);
         Random random = new Random();
         int randomIndex = random.nextInt(listWord.size());
         wordTarget = listWord.get(randomIndex).getWord_target();
         wordMeaning = listWord.get(randomIndex).getWord_explain();
         wordPlay = "";
+        numsOfLetter = wordTarget.length();
         int randomChar = random.nextInt(wordTarget.length());
-
-        upAnchorPane.setPrefWidth(wordTarget.length() * (sizeCardX + padding));
+        System.out.println("T1");
+        upAnchorPane.setPrefWidth(wordTarget.length() * (sizeCardX + padding) + padding);
         upAnchorPane.setPrefHeight(sizeCardY + padding * 2);
-        upRectangle.setWidth(wordTarget.length() * (sizeCardX + padding));
+        upRectangle.setWidth(wordTarget.length() * (sizeCardX + padding) + padding);
         upRectangle.setHeight(sizeCardY + padding * 2);
         upAnchorPane.setLayoutX((double) 850 / 2 - upAnchorPane.getPrefWidth() / 2);
 
-        downAnchorPane.setPrefWidth(wordTarget.length() * (sizeCardX + padding));
+        System.out.println("T2");
+        downAnchorPane.setPrefWidth(wordTarget.length() * (sizeCardX + padding) + padding);
         downAnchorPane.setPrefHeight(sizeCardY + padding * 2);
-        downRectangle.setWidth(wordTarget.length() * (sizeCardX + padding));
+        downRectangle.setWidth(wordTarget.length() * (sizeCardX + padding) + padding);
         downRectangle.setHeight(sizeCardY + padding * 2);
         downAnchorPane.setLayoutX((double) 850 / 2 - upAnchorPane.getPrefWidth() / 2);
+        System.out.println("T3");
         for (int i = 0; i < wordTarget.length(); i++) {
             while (checkExists.contains(randomChar)) {
                 randomChar = random.nextInt(wordTarget.length());
@@ -106,6 +145,9 @@ public class ChaoticWord extends GameController implements Initializable {
             wordPlay += Character.toString(wordTarget.charAt(randomChar));
             Pair pair = new Pair(sizeCardX * i + padding * (i + 1), padding);
             position.add(pair);
+            upCheckIndex.add(0);
+            downCheckIndex.add(1);
+            rawAns.add("_");
         }
         int k = 0;
         for (int i = 0; i < wordTarget.length(); i++) {
@@ -127,17 +169,144 @@ public class ChaoticWord extends GameController implements Initializable {
             downAnchorPane.getChildren().add(button1);
             button1.setLayoutX(position.get(i).x);
             button1.setLayoutY(position.get(i).y);
+            button.setStyle("-fx-background-color: #ef85ff;");
+            button1.setStyle("-fx-background-color: #ef85ff;");
+            allButton.add(button1);
+            allButton.add(button);
+            memoryPosition.put(button1, i);
+        }
+    }
+
+    public void replay(ActionEvent event) throws Exception {
+        checkExists = new HashSet<>();
+        position = new ArrayList<>();
+        upCheckIndex = new ArrayList<>();
+        downCheckIndex = new ArrayList<>();
+        memoryPosition = new HashMap<>();
+        rawAns = new ArrayList<>();
+        playButton.setVisible(true);
+        replayButton.setVisible(false);
+        submitButton.setVisible(false);
+        meaningLabel.setVisible(false);
+        meaningTextField.setVisible(false);
+        int size = allButton.size();
+        for (int i = 0; i < size; i++) {
+            JFXButton button = allButton.get(i);
+            if (isButtonInNamedACPane(button, "defaultAnchorpane")) {
+                defaultAnchorpane.getChildren().remove(button);
+            } else if (isButtonInNamedACPane(button, "upAnchorPane")) {
+                upAnchorPane.getChildren().remove(button);
+            } else {
+                downAnchorPane.getChildren().remove(button);
+            }
+        }
+        upRectangle.setWidth(weighD);
+        upRectangle.setHeight(heightD);
+        upAnchorPane.setLayoutX(252);
+        upAnchorPane.setLayoutY(126);
+        downRectangle.setWidth(weighD);
+        downRectangle.setHeight(heightD);
+        downAnchorPane.setPrefWidth(weighD);
+        downAnchorPane.setPrefHeight(heightD);
+        downAnchorPane.setLayoutX(252);
+        downAnchorPane.setLayoutY(290);
+        upAnchorPane.setPrefWidth(weighD);
+        upAnchorPane.setPrefHeight(heightD);
+        allButton = new ArrayList<>();
+    }
+
+    public void submit(ActionEvent event) throws Exception {
+        playerAns = "";
+        int upFirstEmpty = -1;
+        for (int i = 0; i < numsOfLetter; i++) {
+            if (upCheckIndex.get(i) == 0) {
+                upFirstEmpty = i;
+                break;
+            }
+        }
+        if (upFirstEmpty == -1) {
+            for (int i = 0; i < numsOfLetter; i++) {
+                playerAns += rawAns.get(i);
+            }
+            if (playerAns.equals(wordTarget)) {
+                System.out.println(playerAns);
+                System.out.println(wordTarget);
+                meaningTextField.setText(wordMeaning);
+            } else {
+                meaningTextField.setText("Sai rùi má ơi!");
+            }
         }
     }
 
     @FXML
     public void play(ActionEvent event) throws Exception {
-        ;
+        if (isTimelineRunning) {
+            return;
+        }
+        isTimelineRunning = true;
+        JFXButton button = (JFXButton) event.getSource();
+        int upFirstEmpty = -1;
+        for (int i = 0; i < numsOfLetter; i++) {
+            if (upCheckIndex.get(i) == 0) {
+                upFirstEmpty = i;
+                break;
+            }
+        }
+        if (isButtonInNamedACPane(button, "defaultAnchorpane")) {
+            int indexdownACPane = memoryPosition.get(button);
+            double x = button.getLayoutX();
+            double y = button.getLayoutY();
+            System.out.println(x);
+            final int k = (int) ((x - upAnchorPane.getLayoutX() - padding) / (sizeCardX + padding));
+            System.out.println(k);
+            Timeline timeline = new Timeline();
+
+            KeyFrame keyFrame = new KeyFrame(Duration.seconds(0.5),
+                    new KeyValue(button.translateXProperty(), position.get(indexdownACPane).x + downAnchorPane.getLayoutX() - x),
+                    new KeyValue(button.translateYProperty(), position.get(indexdownACPane).y + downAnchorPane.getLayoutY() - y));
+            timeline.getKeyFrames().add(keyFrame);
+            timeline.setOnFinished(e -> {
+                isTimelineRunning = false;
+                upCheckIndex.set(k, 0);
+                rawAns.set(k, "_");
+                downAnchorPane.getChildren().add(button);
+                button.setLayoutX(position.get(indexdownACPane).x);
+                button.setLayoutY(position.get(indexdownACPane).y);
+                button.setTranslateY(0);
+                button.setTranslateX(0);
+            });
+            timeline.play();
+        } else if (isButtonInNamedACPane(button, "downAnchorPane")) {
+            double x = button.getLayoutX() + downAnchorPane.getLayoutX();
+            double y = button.getLayoutY() + downAnchorPane.getLayoutY();
+            downAnchorPane.getChildren().remove(button);
+            defaultAnchorpane.getChildren().add(button);
+            button.setLayoutX(x);
+            button.setLayoutY(y);
+            final int k = upFirstEmpty;
+            System.out.println(k);
+            Timeline timeline = new Timeline();
+
+            KeyFrame keyFrame = new KeyFrame(Duration.seconds(0.5),
+                    new KeyValue(button.translateXProperty(), position.get(upFirstEmpty).x + upAnchorPane.getLayoutX() - x),
+                    new KeyValue(button.translateYProperty(), position.get(upFirstEmpty).y + upAnchorPane.getLayoutY() - y));
+            timeline.getKeyFrames().add(keyFrame);
+            timeline.setOnFinished(e -> {
+                isTimelineRunning = false;
+                upCheckIndex.set(k, 1);
+                rawAns.set(k, button.getText());
+                button.setLayoutX(position.get(k).x + upAnchorPane.getLayoutX());
+                button.setLayoutY(position.get(k).y + upAnchorPane.getLayoutY());
+                button.setTranslateX(0);
+                button.setTranslateY(0);
+            });
+            timeline.play();
+        }
     }
 
-    private boolean isButtonInNamedHBox(JFXButton button, String hboxName) {
-        HBox parentHBox = (HBox) button.getParent();
-        return parentHBox != null && parentHBox.getId().equals(hboxName);
+    private boolean isButtonInNamedACPane(JFXButton button, String anchorPaneName) {
+        AnchorPane anchorPane = (AnchorPane) button.getParent();
+        return anchorPane != null && anchorPane.getId().equals(anchorPaneName);
     }
 
     @Override
